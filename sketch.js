f9ed19942f4cb4b79d7b5daf15aca79b9ab20697
PikachuXMaster <100964766+PikachuXMaster@users.noreply.github.com>
2022-07-03 14:05:12 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,67 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(800, 700);
+
+  
+	engine = Engine.create();
+	world = engine.world;
+    
+	var block1_options ={
+      restitution: 0.5, 
+      friction: 0.02,
+      frictionAir: 0
+	}
+
+	var block2_options ={
+      restitution: 0.7,
+      friction: 0.01,
+      frictionAir: 0.1
+	}
+
+	var block3_options ={
+      restitution: 0.01,      
+	  friction: 1,
+	  frictionAir: 0.3	
+	}
+	//Crie os Corpos Aqui.
+    var plane_options={
+		isStatic : true
+	}
+
+	Engine.run(engine);
+    
+	block1 = Bodies.circle(220,10,10,block1_options);
+	World.add(world,block1);
+
+	block2 = Bodies.rectangle(110,50,10,10,block2_options);
+	World.add(world,block2);
+
+	block3 = Bodies.rectangle(350,50,10,10,block3_options);
+	World.add(world,block3);
+
+	rectMode(CENTER);
+	ellipseMode(RADIUS);
+}
+
+
+function draw() {
+  background(51);
+  Engine.update(engine);
+  drawSprites();
+  ellipse(block1.position.x,block1.position.y,10);
+  rect(block2.position.x,block2.position.y,10,10);
+  rect(block3.position.x,block3.position.y,10,10);
+}
+
+
+
